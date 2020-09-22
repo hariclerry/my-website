9347626a6fad08f5942798ebc40854bcee8f9c0e
Harriet Ayugi <clerryha@gmail.com>
2020-09-23 02:49:00 +0300

    setup site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,41 @@
-const config = require("./data/siteConfig");
-
 module.exports = {
   siteMetadata: {
-    title: config.siteTitle,
-    description: config.siteDescription,
-    author: config.authorName,
-    ...config
+    title: `Harriet's Blog`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
-  pathPrefix: config.pathPrefix,
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: config.siteTitle,
-        short_name: config.siteTitle,
-        start_url: config.pathPrefix,
-        background_color: config.background_color,
-        theme_color: config.theme_color,
-        display: config.display,
-        icon: config.icon,
-      },
-    },
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: config.googleAnalyticsId,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,133 +1,68 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Container, Row, Col } from 'react-awesome-styled-grid'
-import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter } from "react-icons/fa"
-import siteConfig from '../../data/siteConfig'
-import { withPrefix } from "gatsby"
-import loadable from '@loadable/component'
+import React from "react"
+import { Link, graphql } from "gatsby"
 
-import Hero from '../components/hero'
-import SEO from '../components/SEO'
-import Wrapper from '../components/wrapper'
-import About from '../components/about'
-import Skills from '../components/skills'
-import Timeline from '../components/timeline'
-import Repositories from '../components/repositories'
+import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
+import styled from "styled-components"
 
-const Layout = loadable(() => import('../components/layout'))
-
-const Separator = styled.hr`
-  margin-top: 24px;
-  margin-bottom: 16px;
-  background-color: ${({ theme }) => theme.colors.fontColor};
-  opacity: .2;
+const Title = styled.h1`
+  display: inline-block;
 `
 
-const Home = ({ className, location }) => {
-  // validate siteConfig settings
-  if (siteConfig.googleAnalyticsId === 'UA-000000000-1') {
-    console.error('WARNING: Please set a proper googleAnalyticsId. See https://analytics.google.com for details.');
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  &:hover {
+    color: #1dcaff;
   }
+`
 
-  const title = siteConfig.siteTitle
-  const { keywords } = siteConfig
-  return (
-    <Layout location={location}>
-      <SEO
-        title={title}
-        keywords={keywords}
-      />
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
 
-      <Hero
-        heroImg={siteConfig.siteCover}
-        title={title}
-      />
+const BlogBody = styled.div`
+  margin-bottom: 50px;
+`
 
-      <Wrapper className={className} >
-        <Container className="page-content" fluid>
-          <Row>
-            <Col xs={4} className='avatar'>
-              <img
-                className='avatar__image'
-                src={withPrefix(siteConfig.authorAvatar)}
-                alt='user avatar'
-              />
-              <div className="social">
-                {siteConfig.social.github && <a className="social-link github" href={siteConfig.social.github}>
-                  <FaGithub className="social-icon" size="32" />
-                </a>}
-                {siteConfig.social.linkedin && <a className="social-link linkedin" href={siteConfig.social.linkedin}>
-                  <FaLinkedin className="social-icon" size="32" />
-                </a>}
-                {siteConfig.social.twitter && <a className="social-link twitter" href={siteConfig.social.twitter}>
-                  <FaTwitter className="social-icon" size="32" />
-                </a>}
-                {siteConfig.social.email && <a className="social-link email" href={`mailto:${siteConfig.social.email}`}>
-                  <FaEnvelope className="social-icon" size="32" />
-                </a>}
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={4} sm={4}>
-              <About title='About' text={siteConfig.authorDescription}/>
-            </Col>
-            <Col xs={4} sm={4}>
-              <Skills title='Skills' skills={siteConfig.skills} />
-            </Col>
-          </Row>
-          <Separator />
-          <Timeline />
-          <Separator />
-          {siteConfig.githubUsername && <Repositories />}
-        </Container>
-      </Wrapper>
-    </Layout>
-  )
-}
+export default ({ data }) => (
+  <Layout>
+    <div>
+      <Title>Thoughts by Harriet</Title>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <BlogBody key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>
+              {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+            </BlogTitle>
+          </BlogLink>
+          <p>{node.frontmatter.description || node.excerpt}</p>
+        </BlogBody>
+      ))}
+    </div>
+  </Layout>
+)
 
-export default styled(Home)`
-  .page-content {
-    max-width: 100%;
-    margin-bottom: 40px;
-  }
-
-  .avatar {
-    align-items: center;
-    margin-bottom: 24px;
-    flex-direction: column;
-  }
-
-  .avatar__image {
-    box-shadow: 3px 3px 15px 0px rgba(0,0,0,0.75);
-    max-width: 200px;
-    border-radius: 100px;
-    margin: 0 auto 24px;
-  }
-
-  .social {
-    margin-top: 12px;
-    margin-bottom: 12px;
-  }
-
-  .social-link {
-    padding: 8px;
-    color: #555;
-  }
-
-  a.social-link.twitter:hover {
-    color: #1da1f2;
-  }
-
-  a.social-link.github:hover {
-    color: #24292e;
-  }
-
-  a.social-link.linkedin:hover {
-    color: #0077B5;
-  }
-
-  a.social-link.email:hover {
-    color: #c23a2b;
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            description
+          }
+          fields {
+            slug
+          }
+          excerpt(truncate: true)
+        }
+      }
+    }
   }
 `
